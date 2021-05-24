9023605fcc6398a8d124b296c6219bb38371b813
trphuongnam <phuongnamgroupers@gmail.com>
2021-05-24 16:56:03 +0700

    fix menu_left product page

--- drinkfood/resources/views/public/layouts/elements/content_left.blade.php
+++ drinkfood/resources/views/public/layouts/elements/content_left.blade.php
@@ -2,19 +2,19 @@
     <div class="templatemo_content_left_section">
         <a href="#"><h1>{{ trans('message.foods') }}</h1></a>
         <ul>
-            <li><a href="#">{{ trans('message.com') }}</a></li>
-            <li><a href="#">{{ trans('message.pho') }}</a></li>
-            <li><a href="#">{{ trans('message.bun') }}</a></li>
-            <li><a href="#">{{ trans('message.chao') }}</a></li>
-            <li><a href="#">{{ trans('message.an-vat') }}</a></li>
+            <li><a href="#">Cơm</a></li>
+            <li><a href="#">Bún</a></li>
+            <li><a href="#">Phở</a></li>
+            <li><a href="#">Cháo</a></li>
+            <li><a href="#">Ăn vặt</a></li>
         </ul>
     </div>
     <div class="templatemo_content_left_section">
         <a href="#"><h1>{{ trans('message.drinks') }}</h1></a>
         <ul>
-            <li><a href="#">{{ trans('message.cafe') }}</a></li>
-            <li><a href="#">{{ trans('message.tra-sua') }}</a></li>
-            <li><a href="#">{{ trans('message.nuoc') }}</a></li>
+            <li><a href="#">Cafe</a></li>
+            <li><a href="#">Trà sữa</a></li>
+            <li><a href="#">Nước giải khát</a></li>
         </ul>
     </div>
 </div>
