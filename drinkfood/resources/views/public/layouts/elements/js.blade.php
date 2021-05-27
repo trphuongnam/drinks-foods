900ccc7f09783385e2f9f7206ab982f213b21f47
trphuongnam <phuongnamgroupers@gmail.com>
2021-05-27 17:28:40 +0700

    create regist user page

--- drinkfood/resources/views/public/layouts/elements/js.blade.php
+++ drinkfood/resources/views/public/layouts/elements/js.blade.php
@@ -1,2 +1,4 @@
 <script type="text/javascript" src="{{asset('js/jquery-3.5.1.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/jquery.validate.min.js')}}"></script>
+<script src="{{asset('js/check_validate.js')}}"></script>
 <script src="{{asset('js/app.js')}}"></script>
